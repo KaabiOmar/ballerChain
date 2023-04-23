8a6554b424c153ea6a7397b30c48ad9fe28df78b
KaabiOmar <omar.kaabi@esprit.tn>
2023-04-23 18:13:22 +0100

    in progress2/3.2

--- lib/view/login.dart
+++ lib/view/login.dart
@@ -14,7 +14,6 @@ import 'package:ballerchain/pages//widgets/header_widget.dart';
 class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
 
-
   @override
   _LoginPageState createState() => _LoginPageState();
 }
@@ -32,7 +31,6 @@ class _LoginPageState extends State<LoginPage> {
     super.initState();
     _emailController = TextEditingController();
     _passwordController = TextEditingController();
-
   }
 
   @override
@@ -50,165 +48,182 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Container(
-              height: _headerHeight,
-              child: HeaderWidget(
-                  _headerHeight,
-                  true,
-                  Image.asset(
-                      'assets/images/logo.png')), //let's create a common header widget
+      backgroundColor: Colors.transparent,
+      body: Stack(
+
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/images/bgrnd.png'),
+                fit: BoxFit.cover,
+              ),
             ),
-            SafeArea(
-              child: Container(
-                  padding: EdgeInsets.fromLTRB(20, 10, 20, 10),
-                  margin: EdgeInsets.fromLTRB(
-                      20, 10, 20, 10), // This will be the login form
-                  child: Column(
-                    children: [
-                      Text(
-                        'BallerChain',
-                        style: TextStyle(
-                            fontSize: 50, fontWeight: FontWeight.bold),
-                        textAlign: TextAlign.center,
-                      ),
-                      Text(
-                        'Signin into your account',
-                        style: TextStyle(color: Colors.grey),
-                      ),
-                      SizedBox(height: 30.0),
-                      Form(
-                          key: _formKey,
-                          child: Column(
-                            children: [
-                              Container(
-                                child: TextFormField(
-                                  controller: _emailController,
-                                  decoration: ThemeHelper().textInputDecoration(
-                                      'Email', 'Enter your email'),
-                                ),
-                                decoration:
-                                    ThemeHelper().inputBoxDecorationShaddow(),
-                              ),
-                              SizedBox(height: 30.0),
-                              Container(
-                                child: TextFormField(
-                                  controller: _passwordController,
-                                  obscureText: true,
-                                  decoration: ThemeHelper().textInputDecoration(
-                                      'Password', 'Enter your password'),
-                                ),
-                                decoration:
-                                    ThemeHelper().inputBoxDecorationShaddow(),
-                              ),
-                              SizedBox(height: 15.0),
-                              Container(
-                                margin: EdgeInsets.fromLTRB(10, 0, 10, 20),
-                                alignment: Alignment.topRight,
-                                child: GestureDetector(
-                                  onTap: () {
-                                    Navigator.push(
-                                      context,
-                                      MaterialPageRoute(
-                                          builder: (context) =>
-                                              ForgotPasswordPage()),
-                                    );
-                                  },
-                                  child: Text(
-                                    "Forgot your password?",
-                                    style: TextStyle(
-                                      color: Colors.grey,
+          ),
+          SingleChildScrollView(
+            child: Column(
+              children: [
+                Container(
+                  height: 280,
+                  color: Colors.transparent,
+                  child: Align(
+                    alignment: Alignment.center,
+                    child: Image.asset('assets/images/logo.png',
+                      height: 120, // ajuster la hauteur de l'image
+                      fit: BoxFit.contain),
+                  ),
+                ),
+                SafeArea(
+                  child: Container(
+                      padding: EdgeInsets.fromLTRB(20, 0, 20, 10),
+                      margin: EdgeInsets.fromLTRB(
+                          20, 0, 20, 10), // This will be the login form
+                      child: Column(
+                        children: [
+                          Text(
+                            'BallerChain',
+                            style: TextStyle(
+                                fontSize: 47, fontWeight: FontWeight.bold,color: Colors.white),
+                            textAlign: TextAlign.center,
+                          ),
+                          Text(
+                            'Signin into your account',
+                            style: TextStyle(color: Colors.white70),
+                          ),
+                          SizedBox(height: 30.0),
+                          Form(
+                              key: _formKey,
+                              child: Column(
+                                children: [
+                                  Container(
+                                    child: TextFormField(
+                                      controller: _emailController,
+                                      decoration: ThemeHelper()
+                                          .textInputDecoration(
+                                              'Email', 'Enter your email'),
                                     ),
+                                    decoration: ThemeHelper()
+                                        .inputBoxDecorationShaddow(),
                                   ),
-                                ),
-                              ),
-                              Container(
-                                decoration:
-                                    ThemeHelper().buttonBoxDecoration(context),
-                                child: ElevatedButton(
-                                  style: ThemeHelper().buttonStyle(),
-                                  child: Padding(
-                                    padding:
-                                        EdgeInsets.fromLTRB(40, 10, 40, 10),
-                                    child: Text(
-                                      'Sign In'.toUpperCase(),
-                                      style: TextStyle(
-                                          fontSize: 20,
-                                          fontWeight: FontWeight.bold,
-                                          color: Colors.white),
+                                  SizedBox(height: 30.0),
+                                  Container(
+                                    child: TextFormField(
+                                      controller: _passwordController,
+                                      obscureText: true,
+                                      decoration: ThemeHelper()
+                                          .textInputDecoration('Password',
+                                              'Enter your password'),
                                     ),
+                                    decoration: ThemeHelper()
+                                        .inputBoxDecorationShaddow(),
                                   ),
-                                  onPressed: () async {
-                                    if (_formKey.currentState!.validate()) {
-                                      _formKey.currentState!.save();
-
-                                      await _loginViewModel
-                                          .login(
-                                              context,
-                                              _emailController.text,
-                                              _passwordController.text)
-                                          .then((_) {
-                                        if (_loginViewModel.roleLogin==
-                                            "admin") {
-                                          Navigator.push(
-                                            context,
-                                            MaterialPageRoute(
-                                                builder: (context) =>
-                                                    AdminPage()),
-                                          );
-                                        } else {
-
-                                          Navigator.push(
-                                            context,
-                                            MaterialPageRoute(
-                                                builder: (context) =>
-                                                    LandingPage()),
-                                          );
-                                        }
-
-                                        //  Navigator.pushNamed(context, bottomNavigationRoute);
-                                        // Navigate to success screen
-                                        print(SharedPreference.getUserId()
-                                            .toString());
-                                        print("success !!");
-                                      }).catchError((error) {
-                                        // Handle signup error
-                                      });
-                                    }
-                                  },
-                                ),
-                              ),
-                              Container(
-                                margin: EdgeInsets.fromLTRB(10, 20, 10, 20),
-                                //child: Text('Don\'t have an account? Create'),
-                                child: Text.rich(TextSpan(children: [
-                                  TextSpan(text: "Don\'t have an account? "),
-                                  TextSpan(
-                                    text: 'Create',
-                                    recognizer: TapGestureRecognizer()
-                                      ..onTap = () {
+                                  SizedBox(height: 15.0),
+                                  Container(
+                                    margin: EdgeInsets.fromLTRB(10, 0, 10, 20),
+                                    alignment: Alignment.topRight,
+                                    child: GestureDetector(
+                                      onTap: () {
                                         Navigator.push(
-                                            context,
-                                            MaterialPageRoute(
-                                                builder: (context) =>
-                                                    RegistrationPage()));
+                                          context,
+                                          MaterialPageRoute(
+                                              builder: (context) =>
+                                                  ForgotPasswordPage()),
+                                        );
                                       },
-                                    style: TextStyle(
-                                        fontWeight: FontWeight.bold,
-                                        color: Theme.of(context).accentColor),
+                                      child: Text(
+                                        "Forgot your password?",
+                                        style: TextStyle(
+                                          color: Colors.white70,
+                                        ),
+                                      ),
+                                    ),
                                   ),
-                                ])),
-                              ),
-                            ],
-                          )),
-                    ],
-                  )),
+                                  Container(
+                                    decoration: ThemeHelper()
+                                        .buttonBoxDecoration(context),
+                                    child: ElevatedButton(
+                                      style: ThemeHelper().buttonStyle(),
+                                      child: Padding(
+                                        padding:
+                                            EdgeInsets.fromLTRB(40, 10, 40, 10),
+                                        child: Text(
+                                          'Sign In'.toUpperCase(),
+                                          style: TextStyle(
+                                              fontSize: 20,
+                                              fontWeight: FontWeight.bold,
+                                              color: Colors.white),
+                                        ),
+                                      ),
+                                      onPressed: () async {
+                                        if (_formKey.currentState!.validate()) {
+                                          _formKey.currentState!.save();
+
+                                          await _loginViewModel
+                                              .login(
+                                                  context,
+                                                  _emailController.text,
+                                                  _passwordController.text)
+                                              .then((_) {
+                                            if (_loginViewModel.roleLogin ==
+                                                "admin") {
+                                              Navigator.push(
+                                                context,
+                                                MaterialPageRoute(
+                                                    builder: (context) =>
+                                                        AdminPage()),
+                                              );
+                                            } else {
+                                              Navigator.push(
+                                                context,
+                                                MaterialPageRoute(
+                                                    builder: (context) =>
+                                                        LandingPage()),
+                                              );
+                                            }
+
+                                            //  Navigator.pushNamed(context, bottomNavigationRoute);
+                                            // Navigate to success screen
+                                            print(SharedPreference.getUserId()
+                                                .toString());
+                                            print("success !!");
+                                          }).catchError((error) {
+                                            // Handle signup error
+                                          });
+                                        }
+                                      },
+                                    ),
+                                  ),
+                                  Container(
+                                    margin: EdgeInsets.fromLTRB(10, 20, 10, 20),
+                                    child: Text.rich(TextSpan(children: [
+                                      TextSpan(
+                                          text: "Don\'t have an account? ",style: TextStyle(color: Colors.white70)),
+                                      TextSpan(
+                                        text: 'Create',
+                                        recognizer: TapGestureRecognizer()
+                                          ..onTap = () {
+                                            Navigator.push(
+                                                context,
+                                                MaterialPageRoute(
+                                                    builder: (context) =>
+                                                        RegistrationPage()));
+                                          },
+                                        style: TextStyle(
+                                            fontWeight: FontWeight.bold,
+                                            color:
+                                                Colors.purpleAccent.shade400),
+                                      ),
+                                    ])),
+                                  ),
+                                ],
+                              )),
+                        ],
+                      )),
+                ),
+              ],
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
